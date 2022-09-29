a1b6cfa0c6b943464bd37dd04ac0426e7d1325db
kanyi-a <angela.kanyi@student.moringaschool.com>
2022-09-30 02:03:34 +0300

    done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,37 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+	wrap_parameters format: []
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
   end
+  def show
+    item = Item.find(params[:id])
+      render json: item, status: :ok
+  end
+  
+  def create
+    user = find_user
+    item = user.items.create(item_params)
+    render json: item, status: :created
+  end
+    
 
+  private
+
+  def item_params
+    params.permit(:name, :description, :price)
+  end
+  def render_not_found
+		render json: {error: "We could not find what you are looking for"}, status: :not_found
+	end
+  def find_user
+    User.find(params[:user_id])
+  end
 end
